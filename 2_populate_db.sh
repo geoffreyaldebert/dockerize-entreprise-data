6b1aeec888a3c54328dbe2f15d7531192e97075f
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2020-07-24 18:14:35 +0200

    Add populate db script

--- 2_populate_db.sh
+++ 2_populate_db.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+docker exec -it ${PWD##*/}_web_1 bundle exec rails db:create
+docker exec -it ${PWD##*/}_web_1 bundle exec rails db:migrate
+docker exec -it ${PWD##*/}_web_1 RAILS_ENV=production bundle exec rake sunspot:solr:start
+docker exec -it ${PWD##*/}_web_1 bundle exec rake rna_as_api:import_last_monthly_stocks
+
