f4e8ec44c01fe68be9037fecf1de8342b9f57fcd
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2020-07-24 18:41:44 +0200

    Reorder scripts

---
+++ 2a_populate_db_rna.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker exec -it ${PWD##*/}_rna_1 bundle exec rails db:create
+docker exec -it ${PWD##*/}_rna_1 bundle exec rails db:migrate
+docker exec -it ${PWD##*/}_rna_1 RAILS_ENV=production bundle exec rake sunspot:solr:start
+docker exec -it ${PWD##*/}_rna_1 bundle exec rake rna_as_api:import_last_monthly_stocks
+
+
+
+
+
